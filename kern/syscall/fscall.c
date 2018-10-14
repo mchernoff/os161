f64f9c3111c6de8bc3b47acc04702d62c955d16a
Zhiquan Wang <v3s1b@ssh-linux4.ece.ubc.ca>
2018-10-13 22:03:39 -0700

    Added more Skeleton code for system calls

--- kern/syscall/fscall.c
+++ kern/syscall/fscall.c
@@ -20,10 +20,6 @@
 
 #define UNUSED(x) (void)(x)
 
-int sys_open(const char *filename, int flags);
-int sys_write(int fd, const void *buf, size_t nbytes);
-
-
 int
 sys_open(const char *filename, int flags)
 {
@@ -31,11 +27,60 @@ sys_open(const char *filename, int flags)
 	UNUSED(flags);
 	return 0;
 }
+
 int
 sys_write(int fd, const void *buf, size_t nbytes)
 {
 	UNUSED(fd);
 	UNUSED(buf);
 	UNUSED(nbytes);
+	return 0;
+}
+
+int
+sys_read(int fd, void *buf, size_t nbytes)
+{
+	UNUSED(fd);
+	UNUSED(buf);
+	UNUSED(nbytes);
+	return 0;
+}
+
+int
+sys_close(int fd)
+{
+	UNUSED(fd);
+	return 0;
+}
+
+int
+sys_lseek(int fd, off_t pos, int whence)
+{
+	UNUSED(fd);
+	UNUSED(pos);
+	UNUSED(whence);
+	return 0;
+}
+
+int
+sys_chdir(const char *pathname)
+{
+	UNUSED(pathname);
+	return 0;
+}
+
+int
+sys_dup2(int oldfd, int newfd)
+{
+	UNUSED(oldfd);
+	UNUSED(newfd);
+	return 0;
+}
+
+int
+sys_getcwd(char *buf, size_t buflen)
+{
+	UNUSED(buf);
+	UNUSED(buflen);
 	return 0;
 }
