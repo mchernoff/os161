607c777b354e3215216cbac9f28e5815dc5bb541
Matthew Chernoff <chernoff.matthew@gmail.com>
2018-10-16 00:34:34 -0700

    Changed function names/headers, implemented lseek

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -35,8 +35,7 @@
 #include <thread.h>
 #include <current.h>
 #include <syscall.h>
-#include "../../../syscall/fscall.c"
-
+#include <../../../syscall/fscall.c>
 
 /*
  * System call dispatcher.
@@ -111,11 +110,11 @@ syscall(struct trapframe *tf)
 		break;
 
 		case SYS_open:
-		err = sys_open((char *)tf->tf_a0,(int)tf->tf_a1, (mode_t)tf->tf_a2, (int *)&retval);
+		err = open((char *)tf->tf_a0,(int)tf->tf_a1, (mode_t)tf->tf_a2);
 		break;
 		
 		case SYS_write:
-		err = sys_write((int)tf->tf_a0,(void*)tf->tf_a1,(size_t)tf->tf_a2, (int *)&retval);
+		err = write((int)tf->tf_a0,(void*)tf->tf_a1,(size_t)tf->tf_a2);
 		break;
 		
 	    default:
@@ -132,6 +131,7 @@ syscall(struct trapframe *tf)
 		 */
 		tf->tf_v0 = err;
 		tf->tf_a3 = 1;      /* signal an error */
+		
 	}
 	else {
 		/* Success. */
--- kern/syscall/fscall.c
+++ kern/syscall/fscall.c
@@ -23,11 +23,25 @@
 #include <vnode.h>
 #include <file.h>
 #include <copyinout.h>
+#include <stat.h>
 
 #define UNUSED(x) (void)(x)
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+int	open(const char *filename, int flags, mode_t mode);
+int write(int fd, const void *buf, size_t nbytes);
+off_t read(int fd, void *buf, size_t nbytes);
+int close(int fd);
+off_t lseek(int fd, off_t pos, int whence);
+int chdir(const char *pathname);
+int dup2(int oldfd, int newfd);
+int __getcwd(char *buf, size_t buflen);
+
 /*
-	sys_open opens the file, device, or other kernel object named by the pathname filename. 
+	Opens the file, device, or other kernel object named by the pathname filename. 
 	The flags argument specifies how to open the file and should consist of one of
 
 		O_RDONLY	Open for reading only.
@@ -35,9 +49,8 @@
 		O_RDWR		Open for reading and writing.
 
 */
-int	sys_open(const char *filename, int flags, mode_t mode, int *retval)
+int	open(const char *filename, int flags, mode_t mode)
 {
-	UNUSED(retval);
 
 	//check if filename is valid input
 	if (filename == NULL) {
@@ -96,23 +109,24 @@ int	sys_open(const char *filename, int flags, mode_t mode, int *retval)
 	//set up contents for file descriptor 
 	curproc->p_fd[fd]->flags = flags;
 	curproc->p_fd[fd]->offset = 0;
-	*retval = fd;
 	lock_release(curproc->p_fd[fd]->file_lock);
 	kfree(kfname);
 
-	return 0;
+	return fd;
 }
 
 
 /*
-	sys_write writes up to buflen bytes to the file specified by fd, at the location in the 
+	Writes up to buflen bytes to the file specified by fd, at the location in the 
 	file specified by the current seek position of the file, taking the data from the space 
 	pointed to by buf. The file must be open for writing.
 */
-int sys_write(int fd, const void *buf, size_t nbytes, int *retval)
+int write(int fd, const void *buf, size_t nbytes)
 {
+	int retval;
 	//fd is not a valid file descriptor
-	if (fd < 0 ) {
+	if (fd < 0 || curproc->p_fd[fd] == NULL) {
+		kprintf("failed %d\n", fd);
 		return EBADF;
 	}
 
@@ -122,12 +136,12 @@ int sys_write(int fd, const void *buf, size_t nbytes, int *retval)
 	}
 
 	//if flag is incorrect
-	//if (curproc->p_fd[fd]->flags != O_WRONLY || curproc->p_fd[fd]->flags != O_RDWR)
-	//{
-	//	*retval = -1;
-	//	return EINVAL;
-	//}
+	if (curproc->p_fd[fd]->flags != O_WRONLY || curproc->p_fd[fd]->flags != O_RDWR)
+	{
+		return EINVAL;
+	}
 
+	kprintf("file %d\n", curproc->p_fd[fd]->flags);
 	lock_acquire(curproc->p_fd[fd]->file_lock);		//acquire the lock to the file
 
 	struct uio uio;				//used to manage blocks of data moved around by the kernel
@@ -137,6 +151,7 @@ int sys_write(int fd, const void *buf, size_t nbytes, int *retval)
 	uio_kinit(&iovec, &uio, (void*) buf, nbytes, curproc->p_fd[fd]->offset, UIO_WRITE);
 	iovec.iov_ubase = (userptr_t)buf;			//user-supplied pointer
 	iovec.iov_len = nbytes;						//length of data
+	kprintf("shit\n");
 
 	int result = VOP_WRITE(curproc->p_fd[fd]->fvnode, &uio);		//write data from uio to file at offset
 	if (result == 1) {
@@ -145,16 +160,17 @@ int sys_write(int fd, const void *buf, size_t nbytes, int *retval)
 		return result;
 	}
 
+	kprintf("result %d\n", result);
 	curproc->p_fd[fd]->offset = uio.uio_offset;
 	kfree(buffer);
-	*retval = nbytes - uio.uio_resid;
+	retval = nbytes - uio.uio_resid;
 	lock_release(curproc->p_fd[fd]->file_lock);
 
-	return 0;
+	return retval;
 }
 
-int
-sys_read(int fd, void *buf, size_t nbytes)
+off_t
+read(int fd, void *buf, size_t nbytes)
 {
 	UNUSED(fd);
 	UNUSED(buf);
@@ -163,30 +179,71 @@ sys_read(int fd, void *buf, size_t nbytes)
 }
 
 int
-sys_close(int fd)
+close(int fd)
 {
 	UNUSED(fd);
 	return 0;
 }
 
-int
-sys_lseek(int fd, off_t pos, int whence)
+off_t
+lseek(int fd, off_t pos, int whence)
 {
-	UNUSED(fd);
-	UNUSED(pos);
-	UNUSED(whence);
-	return 0;
+	off_t new_pos;
+	struct stat statbuf;
+	
+	//fd is not a valid file descriptor
+	if (fd < 0 || curproc->p_fd[fd] == NULL) {
+		return EBADF;
+	}
+	
+	// If file system is null, node is not seekable file
+	if(curproc->p_fd[fd]->fvnode->vn_fs == NULL)
+	{
+		return ESPIPE;
+	}
+	lock_acquire(curproc->p_fd[fd]->file_lock);
+	
+	switch(whence){
+		case SEEK_SET:
+			new_pos = pos;
+			break;
+			
+		case SEEK_CUR:
+			new_pos = pos + curproc->p_fd[fd]->offset;
+			break;
+			
+		case SEEK_END:
+			VOP_STAT(curproc->p_fd[fd]->fvnode,&statbuf);
+			new_pos = statbuf.st_size + pos;
+			break;
+			
+		//whence is not valid
+		default:
+			lock_release(curproc->p_fd[fd]->file_lock);
+			return EINVAL;
+	}
+	
+	//Offset is negative
+	if(new_pos < 0 ){
+		lock_release(curproc->p_fd[fd]->file_lock);
+		return EINVAL;
+	}
+	
+	curproc->p_fd[fd]->offset = new_pos;
+	lock_release(curproc->p_fd[fd]->file_lock);
+	
+	return new_pos;
 }
 
 int
-sys_chdir(const char *pathname)
+chdir(const char *pathname)
 {
 	UNUSED(pathname);
 	return 0;
 }
 
 int
-sys_dup2(int oldfd, int newfd)
+dup2(int oldfd, int newfd)
 {
 	UNUSED(oldfd);
 	UNUSED(newfd);
@@ -194,7 +251,7 @@ sys_dup2(int oldfd, int newfd)
 }
 
 int
-sys_getcwd(char *buf, size_t buflen)
+__getcwd(char *buf, size_t buflen)
 {
 	UNUSED(buf);
 	UNUSED(buflen);
